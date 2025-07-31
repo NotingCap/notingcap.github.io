667bb729f1ec45a988fe01b2718a5290a0402320
Sudyordvc94 <chartinai.n@gmail.com>
2025-07-31 10:57:54 +0700

    Build for GitHub Pages

--- midterm-quotation-app/vite.config.js
+++ midterm-quotation-app/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/midterm-quotation-app/', // repo name
+  base: 'notingcap.github.io/midterm-quotation-app/', // repo name
   plugins: [react()],
   build: {
     outDir: 'docs', // this puts built files in the docs/ folder
